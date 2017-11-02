11f96cd0135e948b9d7ba0ffd67129cea8e46777
gregwinther <sebastwi@uio.no>
2017-11-02 21:43:49 +0100

    Implemented fourth order Runge-Kutta

--- 04/forced_oscillation14.py
+++ 04/forced_oscillation14.py
@@ -7,7 +7,9 @@ Sebastian G. Winther-Larsen (2017)
 
 import numpy as np
 from matplotlib import pyplot as plt
-from forced_oscillation import forced_oscillation
+
+# Importing solver
+from rungekutta4 import *
 
 # Constants and parameters
 omega = 100
@@ -17,31 +19,31 @@ k = m*omega*omega
 b = m*omega/Q
 F = 40
 T = 6.0
-parameters = {  "A"   : b/m,
-                "B"   : omega*omega,
-                "C"   : F/m,
-                "D"   : omega,
-                "end" : T/2.0,
-                "func": "forced_oscillation.py" }
+params = {  "A"   : b/m,
+            "B"   : omega*omega,
+            "C"   : F/m,
+            "D"   : omega,
+            "end" : T/2.0,
+            "func": forced_oscillation }
 
 # Number of steps and step size
 N = 2e4
 delta_t = T/N
 
 # Allocating arrays
-y = np.zeros(N)
-v = np.zeros(N)
-t = np.zeros(N)
+y = np.zeros(int(N))
+v = np.zeros(int(N))
+t = np.zeros(int(N))
 
-for i in range(N-1):
+for i in range(int(N-1)):
 
-    # INCOMPLETE!
-    y[i+1], v[i+1], t[i+1] = rk4(y[i], v[j], t[j], delta_t, parameters)
+    y[i+1], v[i+1], t[i+1] = rk4r(y[i], v[i], t[i], delta_t, params)
+
 
 # Plotting
 plt.plot(t, y)
 plt.xlabel("Time (arbitrary unit)")
 plt.ylabel("Oscillation (arbitrary unit)")
-plt.set_xlim([-0.2, T])
-plt.set_ylim([-np.max(A)*1.2, np.max(A)*1.2])
+plt.xlim([-0.2, T])
+plt.ylim([-np.max(y)*1.2, np.max(y)*1.2])
 plt.show()
