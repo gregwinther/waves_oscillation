dd994177a0c6503e9339f254b3595d6689f23db7
gregwinther <sebastwi@uio.no>
2017-11-02 14:22:23 +0100

    Added problem 9 code

---
+++ 05/problem9.py
@@ -0,0 +1,10 @@
+'''
+Code segment for problem 9
+
+Sebastian G. Winther-Larsen (2017)
+'''
+
+import numpy as np
+
+freq = 100 # Frequency in hertz
+x = 0.8 * np.cos(2*np.pi*freq*t) # Simple cosine signal
