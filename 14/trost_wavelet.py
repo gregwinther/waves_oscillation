26404f3c2010272e93f5cbc1e22af51f023fe598
gregwinther <sebastwi@uio.no>
2017-11-02 18:28:11 +0100

    Wavelet analysis now works perfectly

--- 14/trost_wavelet.py
+++ 14/trost_wavelet.py
@@ -14,7 +14,7 @@ from scipy import interpolate
 pbar = ProgressBar()
 
 #data, Fs = sf.read("gjok.wav")
-data, Fs = sf.read("svarttrost2.wav")
+data, Fs = sf.read("svarttrost2.wav", start=17000)
 sd.play(data)
 N = len(data)
 
@@ -23,14 +23,14 @@ fft_transform = np.fft.fft(mono_signal)
 f = np.linspace(0, Fs*(N-1)/N, N)
 nmax = round(N/2)
 
-
+# Plotting signal
 plt.plot(f[0:nmax], np.real(fft_transform[0:nmax]))
 plt.show()
 
 f_min = 1500
 f_max = 4500
 
-K = 32
+K = 96
 
 # Number of analyis frequencies
 M = np.floor(np.log10(f_max / f_min) / np.log10(1 + (1 / (8*K)))) + 1
@@ -39,15 +39,13 @@ f_analysis = f_min
 
 T = N / Fs # Time of sound sample
 t = np.linspace(0, T*(N-1)/N, N)
-# I already have the frequency array
 
 print("M = ", M, " N = ", N)
 WLdiagram = np.zeros((int(M), int(N)))
-# print(WLdiagram)
 f_used = np.zeros(int(M))
 
 # Looping over all frequencies
-print("Performing Wavlet Analysis")
+print("Performing Wavelet Analysis")
 for i in pbar(range(int(M))):
     factor = (K/f_analysis) * (K/f_analysis)
 
@@ -57,7 +55,7 @@ for i in pbar(range(int(M))):
     FTwl = 2 * FTwl
 
     # Computing an entire row in wavelet diagram
-    WLdiagram[i, :] = np.sqrt(np.real(np.fft.ifft(FTwl * fft_transform)))
+    WLdiagram[i, :] = np.sqrt(np.abs(np.fft.ifft(FTwl * fft_transform)))
 
     f_used[i] = f_analysis # Storing saved frequencies
 
@@ -76,18 +74,7 @@ for i in range(int(M)):
         WLdiagram2[i, j] = WLdiagram[i, int(j*P)]
         tP[j] = t[int(j*P)]
 
-print("Interpolating. This may take a while.")
-x = np.arange(0, WLdiagram2.shape[1])
-y = np.arange(0, WLdiagram2.shape[0])
-interpolation_array = np.ma.masked_invalid(WLdiagram2)
-xx, yy = np.meshgrid(x, y)
-x1 = xx[~interpolation_array.mask]
-x2 = yy[~interpolation_array.mask]
-newarr = interpolation_array[~interpolation_array.mask]
-WLdiagram3 = interpolate.griddata((x1, x2), newarr.ravel(), (xx, yy),
-                    method = 'cubic')
-
-plt.imshow(WLdiagram3, extent = [min(tP), max(tP),\
+plt.imshow(WLdiagram2, extent = [min(tP), max(tP),\
         np.log10(min(f_used)), np.log10(max(f_used))],
         aspect = 'auto', cmap="jet")
 plt.show()
