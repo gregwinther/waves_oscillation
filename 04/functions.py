31ea8069a0d5b6d2d8a1eea51c1ac7139b97c41e
gregwinther <sebastwi@uio.no>
2017-11-03 10:10:39 +0100

    Fixed ODE input function

--- 04/functions.py
+++ 04/functions.py
@@ -6,6 +6,9 @@ solution of two coupled ODEs.
 Supposed to model a forced harmonic oscillation where the
 external force only works a given time.
 
+Here one can add other functions as well and change the function that is
+called in the params dictionary in main program.
+
 Sebastian G. Winther-Larsen (2017)
 '''
 
@@ -14,7 +17,7 @@ import numpy as np
 def forced_oscillation(y, v, t, params):
 
     if t < params["end"]:
-        dvdt = - params["A"]*v - params["B"]*np.cos(params["D"]*t)
+        dvdt = - params["A"]*v - params["B"]*y + params["C"]*np.cos(params["D"]*t)
     else:
         dvdt = - params["A"]*v - params["B"]*y
 
