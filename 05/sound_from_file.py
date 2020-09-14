5ffc7f56a375eb4c804ee9e0088ad31bf7bb5f72
gregwinther <sebastwi@uio.no>
2020-09-14 13:09:19 +0200

    Minor adjustments
    	Crop FFT figure by default
    	New file name variable as figure title
    	Black

--- 05/sound_from_file.py
+++ 05/sound_from_file.py
@@ -1,4 +1,4 @@
-'''
+"""
 Example of how to import wave files.
 This is done with pysoundfile, which may be installed, as
 it is not a standard package: 'pip install pysoundfile'
@@ -7,37 +7,41 @@ The sound can be played using sounddevice, install with
 When the file is imported FFT is applied.
 
 Sebastian G. Winther-Larsen(2017)
-'''
+"""
 
 import sounddevice as sd
 import soundfile as sf
 import numpy as np
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
 
 
 # Importing sound file data and samplerate
-N = 2**16
-data, samplerate = sf.read("piccoloHigh.wav", stop = N)
+N = 2 ** 16
+file_name = "piccololow.wav"
+data, samplerate = sf.read(file_name, stop=N)
 
 # Playing sound
 sd.play(data, samplerate)
 
 # The sound file is sample as a stereo signal
 # Extracting only mono signal
-mono_signal = data[:,0]
+mono_signal = data[:, 0]
 
 # FFT
-fft_spectrum = (1.0/N) * np.fft.fft(mono_signal)
+fft_spectrum = (1.0 / N) * np.fft.fft(mono_signal)
 
 # Frequency array for plotting
-freq = (samplerate/2) * np.linspace(0, 1, int(N/2))
+freq = (samplerate / 2) * np.linspace(0, 1, int(N / 2))
+real_fft_array = np.real(fft_spectrum[0 : int(len(fft_spectrum) / 2)])
 
 # plotting
 plt.figure()
 plt.subplot(211)
-plt.title("FFT of Piccolo Flute")
-plt.plot(mono_signal, linewidth = 1) 
+plt.title(file_name)
+plt.plot(mono_signal, linewidth=1)
 plt.subplot(212)
-plt.plot(freq, np.real(fft_spectrum[0:int(len(fft_spectrum)/2)]), \
-        linewidth = 1)
+plt.title("FFT")
+plt.plot(freq, real_fft_array, linewidth=1)
+plt.xlim([0, 5000])
+plt.ylim([0, max(real_fft_array)])
 plt.show()
